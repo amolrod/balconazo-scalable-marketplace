3bcc27a3fd653d95e634cd42fdac965c154439e6
angeelmr <angelmolinarodriguez15@gmail.com>
2025-10-24 13:48:04 +0200

    Nueva arquitectura: 3 microservicios

---
+++ catalog_microservice/src/test/java/com/balconazo/catalog_microservice/CatalogMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balconazo.catalog_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
